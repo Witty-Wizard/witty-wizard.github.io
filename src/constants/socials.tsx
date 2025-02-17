303575d927085619d198f79ee0138157e9cbe4e4
Witty-Wizard <agarwalshashank429+github@gmail.com>
2025-02-18 01:19:42 +0530

    Added: Template Website

---
+++ src/constants/socials.tsx
@@ -0,0 +1,23 @@
+import {
+  IconBrandLinkedin,
+  IconBrandTwitter,
+  IconBrandYoutube,
+} from "@tabler/icons-react";
+
+export const socials = [
+  {
+    href: "",
+    label: "Twitter",
+    icon: IconBrandTwitter,
+  },
+  {
+    href: "",
+    label: "LinkedIn",
+    icon: IconBrandLinkedin,
+  },
+  {
+    href: "",
+    label: "YouTube",
+    icon: IconBrandYoutube,
+  },
+];
