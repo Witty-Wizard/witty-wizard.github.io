5c00c2e07c433e700f686820559e704c2d0e93f4
Witty-Wizard <Witty-Wizard@users.noreply.github.com>
2025-05-01 07:08:18 +0000

    deploy: 05f5fb436c8af09f6cfcc95c791adc292e0b09d1

---
+++ _next/static/chunks/app/page-7f7d66f12cdb0002.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[931],{4193:function(n,_,s){Promise.resolve().then(s.t.bind(s,3542,23))},3542:function(n){n.exports={style:{fontFamily:"'__CalSans_399708', '__CalSans_Fallback_399708'"},className:"__className_399708"}}},function(n){n.O(0,[971,596,744],function(){return n(n.s=4193)}),_N_E=n.O()}]);
